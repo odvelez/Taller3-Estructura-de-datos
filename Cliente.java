0785af6eb758235a4cbced220c76fea96d036ffd
odvelez <odvelez@uninorte.edu.co>
2026-03-09 15:14:29 +0000

    Clases realizadas

---
+++ Cliente.java
@@ -0,0 +1,26 @@
+public class Cliente{
+    private int id;
+    private String nombre;
+    private String email;
+
+    public Usuario(int id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNombre(){
+      return nombre;   
+    }
+   public String getEmail(){
+     return email; 
+   }
+
+    public String toString() {
+        return id + "," + nombre + "," + email;
+    }
+}
